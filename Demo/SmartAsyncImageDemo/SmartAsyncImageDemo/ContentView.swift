3c07daffc20f468139541fb31f34fbfcdcdab213
gentle-giraffe-apps <gentlegiraffeapps@gmail.com>
2025-12-08 11:49:24 -0800

    [chore] create empty demo project

---
+++ Demo/SmartAsyncImageDemo/SmartAsyncImageDemo/ContentView.swift
@@ -0,0 +1,19 @@
+//  Jonathan Ritchey
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/SmartAsyncImageDemo/SmartAsyncImageDemoTests/SmartAsyncImageDemoTests.swift
@@ -0,0 +1,12 @@
+//  Jonathan Ritchey
+
+import Testing
+@testable import SmartAsyncImageDemo
+
+struct SmartAsyncImageDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
